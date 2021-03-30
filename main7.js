4e9ff10b92d153323369c4398aa50c3483b4e82d
Faiza <berrichif0717@gmail.com>
2021-03-30 15:05:25 +0200

    ajout des bouttons

--- main7.js
+++ main7.js
@@ -1,5 +1,13 @@
 
   
-  var y =prompt("veuillez entrer un nombre");
+  /*var y =prompt("veuillez entrer un nombre");
   var x = prompt("veuille entrer un nombre");
   alert("le résultat de multiplication" +y*x);
+*/
+
+function multi (){
+  var myNumber = document.getElementById("number1").value;
+  var myNumber2 = document.getElementById("number2").value;
+  alert(myNumber*myNumber2);
+}
+
